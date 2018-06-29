f883fc02118d29ae7ecd3e9c0976d1ffbbc700a5
Felipe Griep <griep.felipe@gmail.com>
2018-06-29 15:48:21 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/felipegriep/cursomc/domain/Endereco.java
+++ src/main/java/com/felipegriep/cursomc/domain/Endereco.java
@@ -7,8 +7,8 @@ import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
-public class Endereco implements Serializable {
-    private static final long serialVersionUID = 1L;
+    public class Endereco implements Serializable {
+        private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/felipegriep/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,9 @@
+package com.felipegriep.cursomc.repositories;
+
+import com.felipegriep.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+}
