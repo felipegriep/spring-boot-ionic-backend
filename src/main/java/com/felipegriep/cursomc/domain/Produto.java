2f080b6cfdfec924157eef7645d67cdc7b12f9c1
Felipe Griep <griep.felipe@gmail.com>
2018-06-29 16:56:58 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/felipegriep/cursomc/domain/Produto.java
+++ src/main/java/com/felipegriep/cursomc/domain/Produto.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 public class Produto implements Serializable {
@@ -27,6 +25,9 @@ public class Produto implements Serializable {
     )
     private List<Categoria> categorias = new ArrayList<>();
 
+    @OneToMany(mappedBy = "id.produto")
+    private Set<ItemPedido> itens = new HashSet<>();
+
     public Produto() {
 
     }
@@ -35,6 +36,15 @@ public class Produto implements Serializable {
         this.id = id;
         this.nome = nome;
         this.preco = preco;
+    }
+
+    public List<Pedido> getPedidos() {
+        List<Pedido> pedidos = new ArrayList<>();
+        for (ItemPedido item :
+                itens) {
+            pedidos.add(item.getPedido());
+        }
+        return pedidos;
     }
 
     public Integer getId() {
@@ -67,6 +77,14 @@ public class Produto implements Serializable {
 
     public void setCategorias(List<Categoria> categorias) {
         this.categorias = categorias;
+    }
+
+    public Set<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(Set<ItemPedido> itens) {
+        this.itens = itens;
     }
 
     @Override
