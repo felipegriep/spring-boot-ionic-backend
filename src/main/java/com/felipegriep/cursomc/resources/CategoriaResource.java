8e9d0a896410683ab804d0e7e6f54967bbdf17ef
Felipe Griep <griep.felipe@gmail.com>
2018-07-06 16:53:26 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/felipegriep/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/felipegriep/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,6 @@
 package com.felipegriep.cursomc.resources;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.felipegriep.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +18,9 @@ public class CategoriaResource {
 	private CategoriaService service;
 
 	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 
-	    Categoria obj = service.buscar(id);
+	    Categoria obj = service.find(id);
 	    return ResponseEntity.ok().body(obj);
 	}
 
@@ -33,4 +31,11 @@ public class CategoriaResource {
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+	    obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
 }
