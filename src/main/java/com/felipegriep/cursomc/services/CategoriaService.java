f434a447f3f4fc11722db167df0e8948e594d67d
Felipe Griep <griep.felipe@gmail.com>
2018-07-25 13:58:24 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/felipegriep/cursomc/services/CategoriaService.java
+++ src/main/java/com/felipegriep/cursomc/services/CategoriaService.java
@@ -7,6 +7,9 @@ import com.felipegriep.cursomc.services.exceptions.ObjectNotFoundException;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 
@@ -46,5 +49,10 @@ public class CategoriaService {
 
     public List<Categoria> findAll() {
         return repo.findAll();
+    }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = new PageRequest(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
     }
 }
