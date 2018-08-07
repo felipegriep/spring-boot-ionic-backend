da1d7e619a2ef19ed0f0e32aef0869709867f974
Felipe Griep <griep.felipe@gmail.com>
2018-08-07 17:34:58 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/felipegriep/cursomc/resources/ClienteResource.java
+++ src/main/java/com/felipegriep/cursomc/resources/ClienteResource.java
@@ -1,15 +1,20 @@
 package com.felipegriep.cursomc.resources;
 
+import com.felipegriep.cursomc.domain.Categoria;
 import com.felipegriep.cursomc.domain.Cliente;
 import com.felipegriep.cursomc.domain.Cliente;
+import com.felipegriep.cursomc.dto.CategoriaDTO;
 import com.felipegriep.cursomc.dto.ClienteDTO;
+import com.felipegriep.cursomc.dto.ClienteNewDTO;
 import com.felipegriep.cursomc.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,6 +30,15 @@ public class ClienteResource {
 
 	    Cliente obj = service.find(id);
 	    return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
