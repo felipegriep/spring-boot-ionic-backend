12ef24e71a87f34b9a3756db910ccde19924738a
Felipe Griep <griep.felipe@gmail.com>
2018-08-08 17:41:03 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/felipegriep/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/felipegriep/cursomc/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package com.felipegriep.cursomc.dto;
 
 import com.felipegriep.cursomc.domain.Cliente;
+import com.felipegriep.cursomc.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO  implements Serializable {
     private static final long serialVersionUID = 1L;
 
