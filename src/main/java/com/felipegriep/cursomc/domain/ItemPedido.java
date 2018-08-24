5544159a91484e63c45608b21484dae9199982de
Felipe Griep <griep.felipe@gmail.com>
2018-08-24 12:56:41 -0300

    Subtotal e total do Pedido

--- src/main/java/com/felipegriep/cursomc/domain/ItemPedido.java
+++ src/main/java/com/felipegriep/cursomc/domain/ItemPedido.java
@@ -38,6 +38,10 @@ public class ItemPedido implements Serializable {
         this.id = id;
     }
 
+    public double getSubtotal() {
+        return (preco - desconto) * quantidade;
+    }
+
     @JsonIgnore
     public Pedido getPedido() {
         return this.id.getPedido();
